bdf25547bb03bec30691369010220ee0ac2add2e
Graham Cox <graham@grahamcox.co.uk>
2014-11-25 21:37:58 +0000

    Express web server

--- src/main/main.js
+++ src/main/main.js
@@ -1,1 +1,14 @@
-console.log("Hello, World");
+var express = require("express");
+
+var app = express();
+
+app.get("/", function(req, res) {
+    res.send("Hello, World!");
+});
+
+var server = app.listen(3000, function() {
+    var host = server.address().address,
+        port = server.address().port;
+
+    console.log("Listening on http://%s:%s", host, port);
+});
