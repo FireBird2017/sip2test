48d8882a6e812654c8ede9e33bd817adb1d78697
Graham Cox <graham@grahamcox.co.uk>
2014-11-26 16:40:56 +0000

    Message Schema re-written with defineProperties for the main props, and with Symbol keys for the private props

--- src/node/sip2/messageSchema.js
+++ src/node/sip2/messageSchema.js
@@ -1,16 +1,32 @@
+var Symbol = require("symbol");
+
+var fixedParametersKey = Symbol("fixedParameters");
+var namedParametersKey = Symbol("namedParameters");
+
 function MessageSchema(id, name) {
-    this.id = id;
-    this.name = name;
-    this.fixedParameters = [];
-    this.namedParameters = [];
+    Object.defineProperties(this, {
+        name: {
+            value: name,
+            writable: false,
+            enumerable: true
+        }, 
+        id: {
+            value: id,
+            writable: false,
+            enumerable: true
+        }
+    });
+
+    this[fixedParametersKey] = [];
+    this[namedParametersKey] = [];
 }
 
 MessageSchema.prototype = {
     withFixedParameter: function(name, size, type) {
-        this.fixedParameters.push(new FixedParameter(name, size, type));
+        this[fixedParametersKey].push(new FixedParameter(name, size, type));
     },
     withNamedParameter: function(name, key, type) {
-        this.namedParameters.push(new NamedParameter(name, key, type));
+        this[namedParametersKey].push(new NamedParameter(name, key, type));
     }
 }
 
