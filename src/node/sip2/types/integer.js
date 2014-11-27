6e9756af37961c692235c56792b93ada4a42767f
Graham Cox <graham@grahamcox.co.uk>
2014-11-27 21:28:09 +0000

    Added a type representation for integers

---
+++ src/node/sip2/types/integer.js
@@ -0,0 +1,64 @@
+var util = require("util");
+
+function IntegerType() {
+    Object.defineProperties(this, {
+        min: {
+            enumerable: true,
+            writable: true,
+            value: undefined
+        },
+        max: {
+            enumerable: true,
+            writable: true,
+            value: undefined
+        }
+    });
+}
+
+IntegerType.prototype = {
+    validateInput: function(v) {
+        var errors = [];
+        if (!(typeof(v) == "number" && v % 1 === 0)) {
+            errors.push({
+                key: "type_mismatch",
+                message: util.format("Value is of incorrect type. Expected an integral 'number' but got '%s'",
+                    typeof(v))
+            });
+        } else {
+            // It's a Integer, so check everything else about it
+            if (this.min !== undefined && v < this.min) {
+                errors.push({
+                    key: "min",
+                    message: util.format("Value is too low. Min Value is %d but value is %d", 
+                        this.min, v.length)
+                });
+            }
+            if (this.max !== undefined && v > this.max) {
+                errors.push({
+                    key: "max",
+                    message: util.format("Value is too high. Max Value is %d but value is %d", 
+                        this.max, v.length)
+                });
+            }
+        }
+        return errors;
+    },
+    withMin: function(v) {
+        this.min = v;
+        return this;
+    },
+    withoutMin: function() {
+        this.min = undefined;
+        return this;
+    },
+    withMax: function(v) {
+        this.max = v;
+        return this;
+    },
+    withoutMax: function() {
+        this.max = undefined;
+        return this;
+    }
+};
+
+module.exports = IntegerType;
---
+++ src/node/sip2/types/integer_spec.js
@@ -0,0 +1,64 @@
+require("es6-shim");
+
+var should = require("should"),
+    IntegerType = require("./integer");
+
+require("./should_contains_error");
+
+describe("sip2/types/integer", function() {
+    describe("Validation", function() {
+        describe("No restrictions", function() {
+            var type = new IntegerType();
+            it("Should accept the integer 5", function() {
+                type.validateInput(5).should.be.an.Array
+                    .and.be.empty;
+            });
+            it("Should not accept the number 5.2", function() {
+                type.validateInput(5.2).should.be.an.Array
+                    .and.not.be.empty
+                    .and.contains_error("type_mismatch");
+            });
+            it("Should not accept the string '000'", function() {
+                type.validateInput("000").should.be.an.Array
+                    .and.not.be.empty
+                    .and.contains_error("type_mismatch");
+            });
+        });
+        describe("Min Value", function() {
+            var type = new IntegerType()
+                .withMin(3);
+            it("Should accept the integer 3", function() {
+                type.validateInput(3).should.be.an.Array
+                    .and.be.empty;
+            });
+            it("Should accept the integer 4", function() {
+                type.validateInput(4).should.be.an.Array
+                    .and.be.empty;
+            });
+            it("Should not accept the integer 2", function() {
+                type.validateInput(2).should.be.an.Array
+                    .and.not.be.empty
+                    .and.contains_error("min");
+            });
+        });
+        describe("Max Value", function() {
+            var type = new IntegerType()
+                .withMax(3);
+            it("Should accept the integer 3", function() {
+                type.validateInput(3).should.be.an.Array
+                    .and.be.empty;
+            });
+            it("Should accept the integer 2", function() {
+                type.validateInput(2).should.be.an.Array
+                    .and.be.empty;
+            });
+            it("Should not accept the integer 4", function() {
+                type.validateInput(4).should.be.an.Array
+                    .and.not.be.empty
+                    .and.contains_error("max");
+            });
+        });
+    });
+});
+
+
